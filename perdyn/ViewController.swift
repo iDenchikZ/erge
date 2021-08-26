d92c83d7a515e4ac0b04ffae7211e983d847d11e
iDenchikZ <denis.osheretianiy@gmail.com>
2021-08-26 20:51:48 +0300

    started

---
+++ perdyn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  perdyn
+//
+//  Created by cube on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button.layer.cornerRadius = 40
+    }
+    
+    
+
+
+}
+
